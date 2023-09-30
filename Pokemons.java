e3b17f622679003ad9326051b954933248dc7f02
maty <matysta@outlook.com>
2023-09-30 09:02:03 -0700

    Pokemons V7 version.

--- Pokemons.java
+++ Pokemons.java
@@ -7,16 +7,31 @@ public class Main {
         //MAIN POKEMON VARIABLES SETUP
 
         String pokemonName;
-        String pokemonType;
-        String pokemonImage;
         String pokemonClass;
         String pokemonBackroundColour;
         String pokemonFontColour;
-        String pokemonAbility1;
-        String pokemonAbility2;
+        
+        //STAGE 1 POKEMON VARIABLES SETUP
+
+        String stage1PokemonImage;
+        String stage1PokemonType;
+        String stage1PokemonAbility1;
+        String stage1PokemonAbility2;
+        int stage1PokemonStageCount;
+
+        //STAGE 2 POKEMON VARIABLES SETUP
+
+        String stage2PokemonImage = " ";
+        String stage2PokemonType = " ";
+        String stage2PokemonAbility1 = " ";
+        String stage2PokemonAbility2 = " ";
+        int stage2PokemonStageCount = 0;
 
         //PICHU VARIABLES
 
+        int pichuStageCount = 1;
+        String pichuAbility1 = "\u001b[43m" + "\u001b[30m" + "collect" + "\u001B[0m";   
+        String pichuAbility2 = "\u001b[43m" + "\u001b[30m" + "cry for help" + "\u001B[0m";
         String pichuImageLine1 = "        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣴⣶⣿";
         String pichuImageLine2 = "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣴⣾⣿⣿⣿⣿⣿";
         String pichuImageLine3 = "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⣿⠟⢻⣿⣿⣿⣿";
@@ -39,6 +54,7 @@ public class Main {
 
         //PIKACHU VARIABLES
         
+        int pikachuStageCount = 0;
         String pikachuAbility1 = "\u001b[43m" + "\u001b[30m" + "Lightning rod" + "\u001B[0m";
         String pikachuAbility2 = "\u001b[43m" + "\u001b[30m" + "Thunder" + "\u001B[0m";
         String pikachuImageLine1 = ("⠸⣷⣦⠤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣠⣤⠀⠀⠀");
@@ -60,6 +76,7 @@ public class Main {
         
         //BULBASAUR VARIABLES
 
+        int bulbasaurStageCount = 0;
         String bulbasaurAbility1 = "\u001b[42m" + "\u001b[30m" + "Whine whip" + "\u001B[0m";
         String bulbasaurAbility2 = "\u001b[42m" + "\u001b[30m" + "Razor leaf" + "\u001B[0m";
         String bulbasaurImageLine1  = ("         ⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⣀⣀⡀");
@@ -74,102 +91,166 @@ public class Main {
         String bulbasaurImageLine10  = (" ⠀⠀⠀⠛⠒⠒⠁⠀⠈⠷⡤⠤⠐⠀⠘⠒⠒⠖⠁⠀");
         String bulbasaurImage = bulbasaurImageLine1 + "\n" + bulbasaurImageLine2 + "\n" + bulbasaurImageLine3 + "\n" + bulbasaurImageLine4 + "\n" + bulbasaurImageLine5 + "\n" + bulbasaurImageLine6 + "\n" + bulbasaurImageLine7 + "\n" + bulbasaurImageLine8 + "\n" + bulbasaurImageLine9 + "\n" + bulbasaurImageLine10;
 
-        //POKEMON TYPE SELECT + CLASS ASSIGN
+        //POKEMON TYPE SELECT + CLASS/ABILITY/IMAGE/STAGE COUNT/STAGE 2 VARIABLES ASSIGN
         
-        Scanner pokemonTypeInput = new Scanner(System.in);                                       // Create a Scanner object
+        Scanner pokemonTypeInputScanner = new Scanner(System.in);                                       // Create a Scanner object
         System.out.println("What Pokemon do you select? :");
+        String pokemonTypeInput;
         
         while (true) {
-            pokemonType = pokemonTypeInput.nextLine();                                           // Read user input
-            if (pokemonType.equalsIgnoreCase("Pikachu")) {
-                pokemonType = "Pikachu";
+            pokemonTypeInput = pokemonTypeInputScanner.nextLine();                                           // Read user input
+            if (pokemonTypeInput.equalsIgnoreCase("Pichu")) {
+                stage1PokemonType = "Pichu";
                 pokemonClass = "Electric";
-                pokemonAbility1 = pikachuAbility1;
-                pokemonAbility2 = pikachuAbility2;
-                System.out.println("Pikachu selected!");
+                stage1PokemonAbility1 = pichuAbility1;
+                stage1PokemonAbility2 = pichuAbility2;
+                stage1PokemonStageCount = pichuStageCount;
+                stage1PokemonImage = pichuImage;
+                stage2PokemonImage = pikachuImage;
+                stage2PokemonType = "Pikachu";
+                stage2PokemonAbility1 = pikachuAbility1;
+                stage2PokemonAbility2 = pikachuAbility2;
+                stage2PokemonStageCount = pikachuStageCount;
+                System.out.println("Pichu selected!");
                 break;
             }
-            else if (pokemonType.equalsIgnoreCase("Bulbasaur")) {
-                pokemonType = "Bulbasaur";
+            else if (pokemonTypeInput.equalsIgnoreCase("Bulbasaur")) {
+                stage1PokemonType = "Bulbasaur";
                 pokemonClass = "Seed";
-                pokemonAbility1 = bulbasaurAbility1;
-                pokemonAbility2 = bulbasaurAbility2;
+                stage1PokemonAbility1 = bulbasaurAbility1;
+                stage1PokemonAbility2 = bulbasaurAbility2;
+                stage1PokemonStageCount = bulbasaurStageCount;
+                stage1PokemonImage = bulbasaurImage;
                 System.out.println("Bulbasaur selected!");
                 break;
             }
             else {
-                System.out.println("You can only select out of these Pokemons: \n    Bulbasaur\n    Pikachu");
+                System.out.println("You can only select out of these Pokemons: \n    Bulbasaur\n    Pichu");
             }
         }
 
         //COLOUR ASSIGN
         
-        if (pokemonType.equals("Electric")) {
+        if (pokemonClass.equalsIgnoreCase("Electric")) {
             pokemonBackroundColour = "\u001b[43m";  //yellow
             pokemonFontColour = "\u001b[30m"; //black
         }
-        else {
+        else if (pokemonClass.equalsIgnoreCase("seed")) {
             pokemonBackroundColour = "\u001b[42m"; //green
             pokemonFontColour = "\u001b[30m"; //black
+        }
+        else {
+            pokemonBackroundColour = " COLOUR NOT ITITIALIZED ";
+            pokemonFontColour = " COLOUR NOT ITITIALIZED ";
         }
 
 
         //IMAGE ASSIGN
 
-        if (pokemonType.equalsIgnoreCase("Pikachu")) {
-            pokemonImage = pikachuImage;
-        }
-        else if (pokemonType.equalsIgnoreCase("Bulbasaur")) {
-            pokemonImage = bulbasaurImage;
-        }
-        else {
-            pokemonImage = "Undefined";
-        }
+        // if (pokemonType.equalsIgnoreCase("Pikachu")) {
+        //     pokemonImage = pikachuImage;
+        // }
+        // else if (pokemonType.equalsIgnoreCase("Bulbasaur")) {
+        //     pokemonImage = bulbasaurImage;
+        // }
+        // else {
+        //     pokemonImage = "Undefined";
+        // }
         
-        //NAME CREATION
+        //POKEMON NAME
 
         Scanner pokemonNameInput = new Scanner(System.in);  // Create a Scanner object
-        System.out.println("Name your " + pokemonType + "! :");
+        System.out.println("Name your " + stage1PokemonType + "! :");
         pokemonName = pokemonNameInput.nextLine();  // Read user input
-        System.out.println("Your " + pokemonType + " was succesfuly named " + pokemonName + "!");
+        System.out.println("Your " + stage1PokemonType + " was succesfuly named " + pokemonName + "!");
         
         //NEW POKEMON CLASS CREATION
 
-        Pokemon pokemon1  = new Pokemon(pokemonName, pokemonType, pokemonClass, pokemonImage, pokemonFontColour, pokemonBackroundColour, pokemonAbility1, pokemonAbility2);
-        
-        //COMMAND LINES LOOP
+        Pokemon pokemonStage1 = new Pokemon(pokemonName, stage1PokemonType, pokemonClass, stage1PokemonImage, pokemonFontColour, pokemonBackroundColour, stage1PokemonAbility1, stage1PokemonAbility2, stage1PokemonStageCount);
+        Pokemon pokemonStage2 = new Pokemon(pokemonName, stage2PokemonType, pokemonClass, stage2PokemonImage, pokemonFontColour, pokemonBackroundColour, stage2PokemonAbility1, stage2PokemonAbility2, stage2PokemonStageCount);
 
-        Scanner pokemonCommands = new Scanner(System.in);
-        String commandInput;
+        //COMMAND LINES LOOP FOR STAGE ONE
+
+        boolean b = false;
+        Scanner stage1PokemonCommands = new Scanner(System.in);
+        String commandInputStage1;
         while (true) {
             System.out.println("What sould I do?");
-            commandInput = pokemonCommands.nextLine();
-            if (commandInput.equals("ability 1")) {
-                pokemon1.ability1();
+            commandInputStage1 = stage1PokemonCommands.nextLine();
+            if (commandInputStage1.equals("ability 1")) {
+                pokemonStage1.ability1();
             }
-
-            else if (commandInput.equalsIgnoreCase("ability 2")) {
-                pokemon1.ability2();
+            else if (commandInputStage1.equalsIgnoreCase("ability 2")) {
+                pokemonStage1.ability2();
             }
-
-            else if (commandInput.equalsIgnoreCase("image")) {
-                pokemon1.image();
+            else if (commandInputStage1.equalsIgnoreCase("image")) {
+                pokemonStage1.image();
             }
-
-            else if (commandInput.equalsIgnoreCase("stats")) {
-                pokemon1.stats();
+            else if (commandInputStage1.equalsIgnoreCase("stats")) {
+                pokemonStage1.stats();
             }
-
-            else if (commandInput.equalsIgnoreCase("close pokemon")) {
+            else if (commandInputStage1.equalsIgnoreCase("close pokemon")) {
                 break;
+            }
+            else if (commandInputStage1.equalsIgnoreCase("evolve")) {
+                if (stage1PokemonStageCount > 0) { //if its bigger, it will pass to the next loop 
+                    b = true;
+                    break;
+                }
+                else {
+                    b = false; //if its smaller, it will say, that the pokemon cant evolve
+                }
             }
 
             else {
                 System.out.println("I didn't understand...");
                 System.out.println("You can only use these commands:");
-                System.out.println("     stats\n     ability 1\n     ability 2 \n     image\n     close pokemon");
+                System.out.println("     stats\n     ability 1\n     ability 2\n     evolve\n     image\n     close pokemon");
             }
         }
+
+        //COMMAND LINES LOOP FOR STAGE TWO
+
+        if (b == true) {    
+            boolean c;
+            Scanner stage2PokemonCommands = new Scanner(System.in);
+            String commandInputStage2;
+            while (true) {
+                System.out.println("What sould I do?");
+                commandInputStage2 = stage2PokemonCommands.nextLine();
+                if (commandInputStage2.equals("ability 1")) {
+                    pokemonStage2.ability1();
+                }
+                else if (commandInputStage2.equalsIgnoreCase("ability 2")) {
+                    pokemonStage2.ability2();
+                }
+                else if (commandInputStage2.equalsIgnoreCase("image")) {
+                    pokemonStage2.image();
+                }
+                else if (commandInputStage2.equalsIgnoreCase("stats")) {
+                    pokemonStage2.stats();
+                }
+                else if (commandInputStage2.equalsIgnoreCase("close pokemon")) {
+                    break;
+                }
+                else if (commandInputStage2.equalsIgnoreCase("evolve")) {
+                    if (stage2PokemonStageCount > 0) {
+                        c = true;
+                        break;
+                    }
+                    else {
+                        c = false;
+                        break;
+                    } 
+                }
+                else {
+                    System.out.println("I didn't understand...");
+                    System.out.println("You can only use these commands:");
+                    System.out.println("     stats\n     ability 1\n     ability 2\n     evolve\n     image\n     close pokemon");
+                }
+            }
+        }
+        else {}
     }
 }
 
@@ -182,8 +263,9 @@ class Pokemon {
     private String pokemonBackroundColour;
     private String pokemonAbility1;
     private String pokemonAbility2;
+    private int pokemonStageCount;
 
-    public Pokemon(String pokemonName, String pokemonType, String pokemonClass, String pokemonImage, String pokemonFontColour, String pokemonBackroundColour, String pokemonAbility1, String pokemonAbility2) {
+    public Pokemon(String pokemonName, String pokemonType, String pokemonClass, String pokemonImage, String pokemonFontColour, String pokemonBackroundColour, String pokemonAbility1, String pokemonAbility2, int pokemonStageCount) {
         this.pokemonName = pokemonName;
         this.pokemonImage = pokemonImage;
         this.pokemonType = pokemonType;
@@ -192,6 +274,7 @@ class Pokemon {
         this.pokemonBackroundColour = pokemonBackroundColour;
         this.pokemonAbility1 = pokemonAbility1;
         this.pokemonAbility2 = pokemonAbility2;
+        this.pokemonStageCount = pokemonStageCount;
     }
     public void stats() {
         String colourReset = "\u001B[0m";
@@ -209,6 +292,19 @@ class Pokemon {
     public void ability2() {
         System.out.println(pokemonType + " " + pokemonName + " uses " + pokemonAbility2 + "!");
     }
+
+    // public void evolve() {
+    //     if (pokemonStageCount > 0) {
+
+    //         System.out.print("Your " + pokemonType);
+    //         //pokemonType = evolvePokemonType;
+    //         System.out.println(" has evolved to " + pokemonType + "!");
+
+    //     }
+    //     else {
+    //         System.out.println(pokemonType + " doesn't evolve further...");
+    //     }
+    // }
 }
 
 //V3 - electric class experiment, ONLY INCLUDE IN V3!!
